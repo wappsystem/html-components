826bff494bbc7db9041d5cc6c473326857ee05a6
Zhiming Zhou <zzm.vmiis@gmail.com>
2025-05-23 10:10:38 +1000

    update

--- student/form.js
+++ student/form.js
@@ -1,21 +1,28 @@
 
 (function(){
-    //Form
-    //**********************************************************
+    //----------------------------------------------------------
+    var _res={}; try{ _res=JSON.parse(`_vm_res_`);}catch(e){console.log(e);} 
     new ResizeObserver($vm.responsive).observe(vm_form_container__vmID);
-    //**********************************************************
-    vm_submit__vmID.addEventListener('click', (e) =>  { 
+    //----------------------------------------------------------
+    var rid=undefined; try{ rid=_res.qp.rid;}catch(e){} 
+    var record=undefined; try{ record=_res.qp.record;}catch(e){} 
+    //----------------------------------------------------------
+    document.getElementById("vm_submit__vmID").addEventListener('click', (e) =>  { 
         e.preventDefault(); e.stopPropagation(); 
         var formValues=$vm.serialize(vm_F__vmID)
-		var req={cmd:'html-web-module',action:"add", form_values:formValues, sid:"_vm_sid_" }
+		var req={cmd:'json-data-add', rid:rid, form_values:formValues, sid:_res.sid }
         console.log(req);
         $vm.request(req).then((res)=>{
             console.log(res);
             if(res.status=="ok"){
-                alert("Successfully Added.");
-                document.getElementById("vm_F__vmID").reset();
+                document.getElementById("vm_submit__vmID").style.display = "none";
+                document.getElementById("vm_message__vmID").textContent="Successfully submitted.";
             }
         });
     });
-    //**********************************************************
+    //----------------------------------------------------------
+    if(record!=undefined){
+        $vm.deserialize(record,vm_F__vmID);
+    }
+    //----------------------------------------------------------
 })();    
